e1725dbf198c3c61c7e7669b058ef66f635294d0
Anianonymous <hegdeanirudh2003@gmail.com>
2023-11-12 20:33:40 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# WeatherSense
+
+I developed an application using flask, html and css.
+This application gives weather info about the city.
+
+## Demo
+
+[Screencast from 01-11-23 10:12:28 AM IST.webm](https://github.com/anirudh-hegde/WeatherSense/assets/105560839/6999459d-e14b-4529-bc20-edc46fbb2ac2)
---
+++ weather_app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+from config import API_KEY
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def weather():
+    api_key = API_KEY
+    city = request.form.get('city')
+
+    if city:
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+        weather_info = requests.get(url)
+
+        if weather_info.json().get('cod') == 404:
+            result = "No city found"
+        else:
+            try:
+                weather = weather_info.json()['weather'][0]['main']
+                temp = round(weather_info.json()['main']['temp']) - 273.15
+                result = f"The weather in {city} is: {weather}<br>The temperature in {city} is: {temp:.2f} ºC"
+            except KeyError:
+                result = "No weather data found"
+    else:
+        result = ""
+
+    return render_template('weather.html', result=result)
+
+app.run(debug=True)
